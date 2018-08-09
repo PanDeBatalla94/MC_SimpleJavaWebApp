109c7a4e9a9db6e4eb5cc8b93932c7ea32663900
Maria Canqui <mariarenee.canqui@gmail.com>
2018-08-09 16:46:29 -0400

    added counter test

---
+++ src/test/java/CounterServletTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+public class CounterServletTest {
+    public CounterServlet counter;
+    @Before
+    public void init() {
+        counter = new CounterServlet();
+    }
+    
+    @Test
+    public void testGetTitle() {    
+        String title = "<title>CounterServlet using doGet() to increment local variable</title>";
+        assertEquals(title, counter.getTitle());
+    }
+
+
+}
